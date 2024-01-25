456271db2ca69a504fb84dd9273f8c6287c5b89f
HumbleG0d <sergiouni2000@gmail.com>
2024-01-25 15:44:21 -0500

    Add decryption logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 import {encriptacion} from "./encriptacion.js";
+import {desencriptacion} from "./desencriptacion.js";
 
 const textarea_input = document.getElementById("section__input")
 const textarea_output = document.getElementById("section__output")
 
-const button = document.getElementById("section__button--encriptar")
+const button_encriptar = document.getElementById("section__button--encriptar")
+
+const button_desencriptar = document.getElementById("section__button--desencriptar")
 
 
-button.addEventListener("click", e => {
+button_encriptar.addEventListener("click", e => {
     const mensage = textarea_input.value;
     const new_mensage = encriptacion(mensage);
+    textarea_output.value = new_mensage;
+    textarea_output.style.height = `${new_mensage.length/3}px`;
+})
+
+button_desencriptar.addEventListener("click", e => {
+    const mensage = textarea_input.value;
+    const new_mensage = desencriptacion(mensage);
     textarea_output.value = new_mensage;
     textarea_output.style.height = `${new_mensage.length/3}px`;
 })
