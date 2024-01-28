5b68367d1926e9ddfe793462ee1667e06d065967
HumbleG0d <sergiouni2000@gmail.com>
2024-01-27 21:08:39 -0500

    Add boxicons and add footer

--- js/textarea.js
+++
@@ -1,6 +0,0 @@
-const textarea = document.querySelector('textarea');
-textarea.addEventListener('keyup', e => {
-    textarea.style.height = 'auto';    
-    let heigth = e.target.scrollHeight;
-    textarea.style.height = `${heigth}px`;
-})
